b8bfc2095807d7a9f15936f474dc7e2dd6cec600
Guiomar Bobadilla <gigiguio@email.com>
2020-12-03 20:40:53 +0100

    Funcionalidad de login con token

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 'use strict'
 var express = require('express');
 var userController = require('../controllers/user');
+var md_auth = require('../middlewares/authenticated');
 
 var api = express.Router();
 //Crear rutas
-api.get('/probando-controlador', userController.prueba);
+api.get('/probando-controlador', md_auth.ensureAuth, userController.prueba);
 api.post('/register', userController.saveUser);
 api.post('/login', userController.loginUser);
 
